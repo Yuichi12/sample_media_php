9503a175ad3892a2fcee1054bc1646c00182729f
Yu <bakararedboy@gmail.com>
2019-03-12 05:46:19 +0900

    タイトルを修正

--- headWithStyle.php
+++ headWithStyle.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width">
   <meta name="description" content="">
-  <title>twitter-sample</title>
+  <title>記事投稿サイト</title>
   <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="./dist/css/style.css">
